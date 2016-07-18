3b5bc08e412ae6bb473cdbbc51b7b8064baab2ed
Denton Liu <liu.denton+github@gmail.com>
2016-07-18 14:52:57 -0400

    Add StateChannel code

--- contracts/StateChannel.sol
+++ contracts/StateChannel.sol
@@ -1,2 +1,85 @@
 contract StateChannel {
+
+	uint nonce = 0;
+	mapping (bytes32 =>
+			mapping (address => bool)) consents;
+	address[] participants;
+
+	modifier onlySelf {
+		if (msg.sender == address(this)) {
+			_
+		} else {
+			throw;
+		}
+	}
+
+	function StateChannel(address[] _participants) {
+		participants = _participants;
+	}
+
+	function eval(
+		uint _nonce,
+		address[] _calledContracts,
+		bytes4[] _methodSignatures,
+		uint[] _argumentLengths,
+		bytes32[] _arguments
+	) {
+		if (
+			_nonce <= nonce ||
+			_calledContracts.length != _methodSignatures.length ||
+			_calledContracts.length != _argumentLengths.length
+		) {
+			throw;
+		}
+
+		uint i;
+		uint j;
+		bytes32 hash = sha3(_nonce, _calledContracts, _methodSignatures, _argumentLengths, _arguments);
+
+		for (i = 0; i < participants.length; i++) {
+			if (!consents[hash][participants[i]]) {
+				throw;
+			}
+		}
+		nonce = _nonce;
+
+		uint argumentsIndex = 0;
+		for (i = 0; i < _calledContracts.length; i++) {
+
+			bytes32[] memory arguments = new bytes32[](_argumentLengths[i]);
+			for (j = 0; j < _argumentLengths[i]; j++) {
+				arguments[j] = _arguments[argumentsIndex++];
+			}
+
+			if (!_calledContracts[i].call(_methodSignatures[i], arguments[i])) {
+				throw;
+			}
+		}
+	}
+
+	function consent(bytes32 _hash) {
+		consents[_hash][msg.sender] = true;
+	}
+
+	function sendEther(address _recipient, uint _amount) external onlySelf {
+		if (!_recipient.send(_amount)) {
+			throw;
+		}
+	}
+
+	function changeParticipants(address[] _participants) external onlySelf {
+		participants = _participants;
+	}
+
+	function clean(bytes32[] _hashes, address[] _participants) external onlySelf {
+		for (uint i = 0; i < _hashes.length; i++) {
+			for (uint j = 0; j < _participants.length; j++) {
+				delete consents[_hashes[i]][_participants[j]];
+			}
+		}
+	}
+
+	function kill(address _recipient) external onlySelf {
+		selfdestruct(_recipient);
+	}
 }
