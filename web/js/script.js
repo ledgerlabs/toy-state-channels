2887b75636d87c5333cd062a33e7dbfd738175c8
Denton Liu <liu.denton+github@gmail.com>
2016-08-19 16:37:03 -0400

    Create initial UI of UnanimousConsent

--- web/js/script.js
+++ web/js/script.js
@@ -6,3 +6,44 @@ if (typeof web3 !== 'undefined') {
 }
 
 var UnanimousConsent;
+
+function addUnanimousConsentParticipant(form) {
+	$(form).find(".participants").append("<li>" +form.address.value +"</li>");
+}
+
+function clearUnanimousConsentParticipants(form) {
+	$(form).find(".participants").empty();
+}
+
+function createUnanimousConsent(form) {
+	var addresses = $(form).find(".participants li").map(
+			function () {
+				return $(this).text();
+			}).get();
+
+//	UnanimousConsent = web3.eth.contract(/*unanimous interface*/).new(
+//			addresses,
+//			{
+//				from: web3.eth.accounts[$('#sender').val()],
+//				data: /*unanimous code*/,
+//				gas: 4700000
+//			},
+//			if (typeof contract.address !== 'undefined') {
+//				// TODO: make this work $('#existingContract input[name=address]').val(contract.address);
+//				localStorage.setItem('unanimousAddress', contract.address);
+//				alert('Contract has been mined at: ' +contract.address);
+//			} else {
+//				alert('Contract transaction: ' +contract.transactionHash);
+//			});
+}
+
+$(document).ready(function () {
+	for (var i = 0; i < web3.eth.accounts.length; i++) {
+		$('.addresses').append("<option value=\"" +i +"\">" +web3.eth.accounts[i] +"</option>");
+	}
+
+	// TODO: Persistent state
+	// if (localStorage.getItem('address')) {
+	// 	$('#existingContract input[name=address]').val(localStorage.getItem('unanimouAddress'));
+	// }
+});
