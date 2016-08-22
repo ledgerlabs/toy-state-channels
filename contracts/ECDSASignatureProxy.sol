5565cea193213a8672d6a51c1afe8ac7230740dd
Denton Liu <liu.denton+github@gmail.com>
2016-08-22 10:56:55 -0400

    Clean up new contract

--- contracts/ECDSASignatureProxy.sol
+++ contracts/ECDSASignatureProxy.sol
@@ -3,22 +3,24 @@ import "UnanimousConsent.sol";
 /**
 * This contract will consent to some hashes on behalf of a specified
 * address if and only if the specified address has signed off on it
-**/
-
+*/
 contract ECDSASignatureProxy {
-    address signer;
-    UnanimousConsent unanimousConsent;
-    
-    function ECDSASignatureProxy(address _signer, UnanimousConsent _unanimousConsent) {
-        signer = _signer;
-        unanimousConsent = _unanimousConsent;
-    }
-    
-    // Checks if all of the hashes have been signed off on
-    function forward(bytes32[] hashes, uint8 v, bytes32 r, bytes32 s) returns(bool) {
-        bytes32 hash = sha3(hashes);
-        if (ecrecover(hash, v, r, s) != signer) return false;
-        unanimousConsent.consent(hashes);
-        return true;
-    }
+	address signer;
+	UnanimousConsent unanimousConsent;
+
+	function ECDSASignatureProxy(address _signer, UnanimousConsent _unanimousConsent) {
+		signer = _signer;
+		unanimousConsent = _unanimousConsent;
+	}
+
+	// Checks if all of the hashes have been signed off on
+	function forward(bytes32[] _hashes, uint8 _v, bytes32 _r, bytes32 _s) external returns (bool) {
+		bytes32 hash = sha3(hashes);
+		if (ecrecover(_hash, _v, _r, _s) != signer) {
+			return false;
+		} else {
+			unanimousConsent.consent(hashes);
+			return true;
+		}
+	}
 }
