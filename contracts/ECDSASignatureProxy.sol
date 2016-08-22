cbdbacf32c5de60115e719a768b806fb9cb30a46
Anna Wang <s067220898@gmail.com>
2016-08-22 10:52:29 -0400

    removed unnecessary for loop

--- contracts/ECDSASignatureProxy.sol
+++ contracts/ECDSASignatureProxy.sol
@@ -15,13 +15,9 @@ contract ECDSASignatureProxy {
     }
     
     // Checks if all of the hashes have been signed off on
-    function forward(bytes32[] hashes, uint8[] v, bytes32[] r, bytes32[] s) returns(bool) {
-        uint length = hashes.length;
-        for (uint i = length - 1; i <= 0; --i) {
-            if (ecrecover(hashes[i], v[i], r[i], s[i]) != signer) {
-                return false;
-            }
-        }
+    function forward(bytes32[] hashes, uint8 v, bytes32 r, bytes32 s) returns(bool) {
+        bytes32 hash = sha3(hashes);
+        if (ecrecover(hash, v, r, s) != signer) return false;
         unanimousConsent.consent(hashes);
         return true;
     }
