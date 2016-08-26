34c457170b8bce8e09cd6c829d963c7aaadfc3b3
Anna Wang <s067220898@gmail.com>
2016-08-26 13:24:58 -0400

    Added event to ease the ui side of things

--- contracts/NonceCompareOpSingleton.sol
+++ contracts/NonceCompareOpSingleton.sol
@@ -2,11 +2,14 @@ import "./NonceCompareOp.sol";
 
 contract NonceCompareOpSingleton {
 	NonceCompareOp nonceCompareOp;
+	
+	event CreateNonceCompareOp(address indexed nonceCompareOp);
 
 	function getNonceCompareOp() returns (NonceCompareOp) {
 		if (address(nonceCompareOp) == 0) {
 			nonceCompareOp = new NonceCompareOp();
 		}
+		CreatedNonceCompareOp(nonceCompareOp);
 		return nonceCompareOp;
 	}
 }
