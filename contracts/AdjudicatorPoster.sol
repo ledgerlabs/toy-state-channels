e1068a2c2f6a50f5914e4fda1c7d6ef8cb4823a5
Denton Liu <liu.denton+github@gmail.com>
2016-08-26 14:46:34 -0400

    Change _hash to _id

--- contracts/AdjudicatorPoster.sol
+++ contracts/AdjudicatorPoster.sol
@@ -4,11 +4,11 @@ import "./BulletinBoard.sol";
 library AdjudicatorPoster {
 	function postAdjudicator(
 		BulletinBoard _bulletinBoard, 
-		bytes32 _hash, 
+		bytes32 _id, 
 		CompareOp _compareOp, 
 		address _owner, 
 		uint _timeout
 	) {
-		_bulletinBoard.updateRegistry(_hash, new Adjudicator(_compareOp, _owner, _timeout));
+		_bulletinBoard.updateRegistry(_id, new Adjudicator(_compareOp, _owner, _timeout));
 	}
 }
