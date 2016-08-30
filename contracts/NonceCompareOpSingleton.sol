7de4a86f5a6ef4f192b574afa8ab11ff5076e3cd
Denton Liu <liu.denton+github@gmail.com>
2016-08-30 15:54:22 -0400

    Revert "Added event to ease the ui side of things"
    This reverts commit 34c457170b8bce8e09cd6c829d963c7aaadfc3b3.
    
    An event shouldn't be necessary for this contract.

--- contracts/NonceCompareOpSingleton.sol
+++ contracts/NonceCompareOpSingleton.sol
@@ -2,14 +2,11 @@ import "./NonceCompareOp.sol";
 
 contract NonceCompareOpSingleton {
 	NonceCompareOp nonceCompareOp;
-	
-	event CreateNonceCompareOp(address indexed nonceCompareOp);
 
 	function getNonceCompareOp() returns (NonceCompareOp) {
 		if (address(nonceCompareOp) == 0) {
 			nonceCompareOp = new NonceCompareOp();
 		}
-		CreatedNonceCompareOp(nonceCompareOp);
 		return nonceCompareOp;
 	}
 }
